ae835f60cbf70db6001f49831cb657613842027a
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-03 16:07:50 -0700

    Trying to call dependency script from sudo.

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:amarburg/opencv3 -y
-sudo apt-get update -q
-sudo apt-get install libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs.dev
+add-apt-repository ppa:amarburg/opencv3 -y
+apt-get update -q
+apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs.dev
