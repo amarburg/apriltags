b0dcf938d9008f683c35c4b93fe1751acb6006d6
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-03 16:04:17 -0700

    Updated script for installing dependencies.

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-apt-get update
+sudo add-apt-repository ppa:amarburg/opencv3 -y
+sudo apt-get update -q
+sudo apt-get install libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs.dev
