d9d25a7dc3da5acac26f137e56c4d33e62038c05
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-11 11:30:22 -0700

    Hm, need to load the top-level libopencv3-dev to get the CMake files, even if it includes all of the submodules.

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -10,10 +10,11 @@ fi
 if [ $OPENCV -eq 2 ]; then
   echo "Using OpenCV 2 from Ubuntu"
   apt-get update -q
-  apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs-dev $COMMON_PKGS
+  apt-get install -y libopencv-dev $COMMON_PKGS
+  #apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs-dev $COMMON_PKGS
 elif [ $OPENCV -eq 3 ]; then
   echo "Using OpenCV 3 from PPA"
   add-apt-repository ppa:amarburg/opencv3 -y
   apt-get update -q
-  apt-get install -y libopencv3-highgui-dev libopencv3-core-dev libopencv3-imgproc-dev libopencv3-imgcodecs-dev $COMMON_PKGS
+  apt-get install -y libopencv3-dev $COMMON_PKGS
 fi
