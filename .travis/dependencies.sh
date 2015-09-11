e7a663ae9ecd8edb0b2b5d23840e609ed09adaeb
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-11 10:08:01 -0700

    Updated package names to pull the Opencv3 packages from my ppa.

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -2,4 +2,4 @@
 
 add-apt-repository ppa:amarburg/opencv3 -y
 apt-get update -q
-apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs-dev
+apt-get install -y libopencv3-highgui-dev libopencv3-core-dev libopencv3-imgproc-dev libopencv3-imgcodecs-dev
