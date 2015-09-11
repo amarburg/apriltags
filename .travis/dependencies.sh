440879da4d2c191ea517b60c8885c7f4bc48ad05
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-11 11:03:17 -0700

    Further iteration on the dependencies script.

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-if [ "$OPENCV" -eq 2 ]; then
+COMMON_PKGS="libeigen3-dev"
+
+if [ -z "$OPENCV" ]; then
+  echo "Must set environment variable OPENCV"
+  exit 1
+fi
+
+if [ $OPENCV -eq 2 ]; then
   echo "Using OpenCV 2 from Ubuntu"
   apt-get update -q
-  apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs-dev
-else
+  apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs-dev $COMMON_PKGS
+elif [ $OPENCV -eq 3 ]; then
   echo "Using OpenCV 3 from PPA"
   add-apt-repository ppa:amarburg/opencv3 -y
   apt-get update -q
-  apt-get install -y libopencv3-highgui-dev libopencv3-core-dev libopencv3-imgproc-dev libopencv3-imgcodecs-dev
+  apt-get install -y libopencv3-highgui-dev libopencv3-core-dev libopencv3-imgproc-dev libopencv3-imgcodecs-dev $COMMON_PKGS
 fi
