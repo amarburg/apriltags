96f2131a6c1fd9840babadb1c61aff2c21bbf436
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-11 09:38:36 -0700

    Fixed package name error in .travis/dependencies.sh

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -2,4 +2,4 @@
 
 add-apt-repository ppa:amarburg/opencv3 -y
 apt-get update -q
-apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs.dev
+apt-get install -y libopencv-highgui-dev libopencv-core-dev libopencv-imgproc-dev libopencv-imgcodecs-dev
