159257f2438d4cd18451250d9bc4e33bf0e80fdc
Aaron Marburg <amarburg@apl.washington.edu>
2015-09-14 15:14:31 -0700

    Added a comment to build.sh about the libtbb.so symlink for precise.

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,10 +8,12 @@ sudo cmake .
 sudo make
 sudo cp *.a /usr/lib
 
+## Strangely, the libtbb2 package for precise doesn't create this symlink.
 if [ ! -f /usr/lib/libtbb.so ]; then
 	sudo ln -s /usr/lib/libtbb.so.2 /usr/lib/libtbb.so
 fi
 
+# Now build out package
 cd $HOME
 mkdir build
 cd build
